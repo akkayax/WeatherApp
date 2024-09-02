2284e033624296ea38cde6364e4307439db750ab
akkayax <zeynepsakkaya2@gmail.com>
2024-09-02 14:51:13 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1 className='text-9xl text-center'>Weather App</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
